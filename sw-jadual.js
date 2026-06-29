e32ffcdccf76c38410a080905830f294af9183f7
gdienjr <gdienjr@gmail.com>
2026-06-29 18:45:47 +0800

    Add files via upload

---
+++ sw-jadual.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "dsat-jadual-v1";
+const urlsToCache = [
+  "/dsat/jadual.html",
+  "/dsat/icon-192.png",
+  "/dsat/icon-512.png"
+];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key) { return key !== CACHE_NAME; })
+            .map(function(key) { return caches.delete(key); })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
